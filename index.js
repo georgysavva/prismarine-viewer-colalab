33c079acbf773f676bdee77881eea827507b402d
Georgy Savva <georgy.savva@gmail.com>
2025-08-22 16:14:42 -0400

    Uncomment headless module import in index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ module.exports = {
   // freecamera: require('./lib/freecamera'),
   freecamera: require('./lib/freecamera-base64'),
   standalone: require('./lib/standalone'),
-  // headless: require('./lib/headless'),
-  headless: require('./lib/headless-base64'),
+  headless: require('./lib/headless'),
+  // headless: require('./lib/headless-base64'),
   viewer: require('./viewer'),
   supportedVersions: require('./viewer').supportedVersions
 }
