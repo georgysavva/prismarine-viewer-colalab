561fe3fa1adb7ff57f1ce592756b94089358f1bf
Georgy <georgy.savva@nyu.edu>
2025-08-26 10:02:38 -0700

    format files

--- viewer/lib/viewer.js
+++ viewer/lib/viewer.js
@@ -1,96 +1,98 @@
-const THREE = require('three')
-const TWEEN = require('@tweenjs/tween.js')
-const { WorldRenderer } = require('./worldrenderer')
-const { Entities } = require('./entities')
-const { Primitives } = require('./primitives')
-const { getVersion } = require('./version')
-const { Vec3 } = require('vec3')
-const assert = require('assert')
+const THREE = require("three");
+const TWEEN = require("@tweenjs/tween.js");
+const { WorldRenderer } = require("./worldrenderer");
+const { Entities } = require("./entities");
+const { Primitives } = require("./primitives");
+const { getVersion } = require("./version");
+const { Vec3 } = require("vec3");
+const assert = require("assert");
 
 class Viewer {
-  constructor (renderer) {
-    this.scene = new THREE.Scene()
-    this.scene.background = new THREE.Color('lightblue')
+  constructor(renderer) {
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color("lightblue");
 
-    this.ambientLight = new THREE.AmbientLight(0xcccccc)
-    this.scene.add(this.ambientLight)
+    this.ambientLight = new THREE.AmbientLight(0xcccccc);
+    this.scene.add(this.ambientLight);
 
-    this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
-    this.directionalLight.position.set(1, 1, 0.5).normalize()
-    this.directionalLight.castShadow = true
-    this.scene.add(this.directionalLight)
+    this.directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+    this.directionalLight.position.set(1, 1, 0.5).normalize();
+    this.directionalLight.castShadow = true;
+    this.scene.add(this.directionalLight);
 
-    const size = renderer.getSize(new THREE.Vector2())
-    this.camera = new THREE.PerspectiveCamera(75, size.x / size.y, 0.1, 1000)
+    const size = renderer.getSize(new THREE.Vector2());
+    this.camera = new THREE.PerspectiveCamera(75, size.x / size.y, 0.1, 1000);
 
-    this.world = new WorldRenderer(this.scene)
-    this.entities = new Entities(this.scene)
-    this.primitives = new Primitives(this.scene, this.camera)
+    this.world = new WorldRenderer(this.scene);
+    this.entities = new Entities(this.scene);
+    this.primitives = new Primitives(this.scene, this.camera);
 
-    this.domElement = renderer.domElement
-    this.playerHeight = 1.6
-    this.isSneaking = false
+    this.domElement = renderer.domElement;
+    this.playerHeight = 1.6;
+    this.isSneaking = false;
   }
 
-  resetAll () {
-    this.world.resetWorld()
-    this.entities.clear()
-    this.primitives.clear()
+  resetAll() {
+    this.world.resetWorld();
+    this.entities.clear();
+    this.primitives.clear();
   }
 
-  setVersion (version) {
-    version = getVersion(version)
+  setVersion(version) {
+    version = getVersion(version);
     if (version === null) {
-      const msg = `${version} is not supported`
-      window.alert(msg)
-      console.log(msg)
-      return false
+      const msg = `${version} is not supported`;
+      window.alert(msg);
+      console.log(msg);
+      return false;
     }
-    console.log('Using version: ' + version)
-    this.version = version
-    this.world.setVersion(version)
-    this.entities.clear()
-    this.primitives.clear()
-    return true
+    console.log("Using version: " + version);
+    this.version = version;
+    this.world.setVersion(version);
+    this.entities.clear();
+    this.primitives.clear();
+    return true;
   }
 
-  addColumn (x, z, chunk) {
-    this.world.addColumn(x, z, chunk)
+  addColumn(x, z, chunk) {
+    this.world.addColumn(x, z, chunk);
   }
 
-  removeColumn (x, z) {
-    this.world.removeColumn(x, z)
+  removeColumn(x, z) {
+    this.world.removeColumn(x, z);
   }
 
-  setBlockStateId (pos, stateId) {
-    this.world.setBlockStateId(pos, stateId)
+  setBlockStateId(pos, stateId) {
+    this.world.setBlockStateId(pos, stateId);
   }
 
-  updateEntity (e) {
-    this.entities.update(e)
+  updateEntity(e) {
+    this.entities.update(e);
   }
 
-  updatePrimitive (p) {
-    this.primitives.update(p)
+  updatePrimitive(p) {
+    this.primitives.update(p);
   }
 
-  setFirstPersonCamera (pos, yaw, pitch) {
+  setFirstPersonCamera(pos, yaw, pitch) {
     if (pos) {
-      let y = pos.y + this.playerHeight
-      if (this.isSneaking) y -= 0.3
-      new TWEEN.Tween(this.camera.position).to({ x: pos.x, y, z: pos.z }, 50).start()
+      let y = pos.y + this.playerHeight;
+      if (this.isSneaking) y -= 0.3;
+      new TWEEN.Tween(this.camera.position)
+        .to({ x: pos.x, y, z: pos.z }, 50)
+        .start();
     }
-    this.camera.rotation.set(pitch, yaw, 0, 'ZYX')
+    this.camera.rotation.set(pitch, yaw, 0, "ZYX");
   }
 
   /**
    * This function will set scene.background, ambientLight, directionalLight, according to time of the day
-   * 
-   * @param {*} tick Time of the day, in ticks. 
+   *
+   * @param {*} tick Time of the day, in ticks.
    * Time is based on ticks, where 20 ticks happen every second. There are 24000 ticks in a day, making Minecraft days exactly 20 minutes long.
    * The time of day is based on the timestamp modulo 24000. 0 is sunrise, 6000 is noon, 12000 is sunset, and 18000 is midnight.
    */
-  setTimeOfDay (tick) {
+  setTimeOfDay(tick) {
     // console.log("timeOfDay: ", tick)
 
     /*
@@ -104,76 +106,85 @@ class Viewer {
 
     if (tick <= 11500) {
       time = 1;
-    } else if(tick <= 14000) {
+    } else if (tick <= 14000) {
       time = 1 - (tick - 11500) / 2500;
-    } else if(tick <= 21500) {
-      time = 0
-    } else if(tick <= 24000) {
+    } else if (tick <= 21500) {
+      time = 0;
+    } else if (tick <= 24000) {
       time = (tick - 21500) / 2500;
-    } else assert(false, "timeOfDay is exceed 24000")
+    } else assert(false, "timeOfDay is exceed 24000");
 
-    const bg_r = Math.floor(173 * time)
-    const bg_g = Math.floor(216 * time)
-    const bg_b = Math.floor(230 * time)
-    const ambient = Math.floor(104 * time + 104)
-    const light = Math.floor(128 * time + 127)
+    const bg_r = Math.floor(173 * time);
+    const bg_g = Math.floor(216 * time);
+    const bg_b = Math.floor(230 * time);
+    const ambient = Math.floor(104 * time + 104);
+    const light = Math.floor(128 * time + 127);
 
-    this.scene.background = new THREE.Color(`rgb(${bg_r}, ${bg_g}, ${bg_b})`)
+    this.scene.background = new THREE.Color(`rgb(${bg_r}, ${bg_g}, ${bg_b})`);
 
-    this.scene.remove(this.ambientLight)
-    this.ambientLight = new THREE.AmbientLight(`rgb(${ambient}, ${ambient}, ${ambient})`)
-    this.scene.add(this.ambientLight)
+    this.scene.remove(this.ambientLight);
+    this.ambientLight = new THREE.AmbientLight(
+      `rgb(${ambient}, ${ambient}, ${ambient})`
+    );
+    this.scene.add(this.ambientLight);
 
-    this.scene.remove(this.directionalLight)
-    this.directionalLight = new THREE.DirectionalLight(`rgb(${light}, ${light}, ${light})`, 0.5)
-    this.directionalLight.position.set(1, 1, 0.5).normalize()
-    this.directionalLight.castShadow = true
-    this.scene.add(this.directionalLight)
+    this.scene.remove(this.directionalLight);
+    this.directionalLight = new THREE.DirectionalLight(
+      `rgb(${light}, ${light}, ${light})`,
+      0.5
+    );
+    this.directionalLight.position.set(1, 1, 0.5).normalize();
+    this.directionalLight.castShadow = true;
+    this.scene.add(this.directionalLight);
   }
 
-  listen (emitter) {
-    emitter.on('entity', (e) => {
-      this.updateEntity(e)
-    })
+  listen(emitter) {
+    emitter.on("entity", (e) => {
+      this.updateEntity(e);
+    });
 
-    emitter.on('primitive', (p) => {
-      this.updatePrimitive(p)
-    })
+    emitter.on("primitive", (p) => {
+      this.updatePrimitive(p);
+    });
 
-    emitter.on('loadChunk', ({ x, z, chunk }) => {
-      this.addColumn(x, z, chunk)
-    })
+    emitter.on("loadChunk", ({ x, z, chunk }) => {
+      this.addColumn(x, z, chunk);
+    });
 
-    emitter.on('unloadChunk', ({ x, z }) => {
-      this.removeColumn(x, z)
-    })
+    emitter.on("unloadChunk", ({ x, z }) => {
+      this.removeColumn(x, z);
+    });
 
-    emitter.on('blockUpdate', ({ pos, stateId }) => {
-      this.setBlockStateId(new Vec3(pos.x, pos.y, pos.z), stateId)
-    })
+    emitter.on("blockUpdate", ({ pos, stateId }) => {
+      this.setBlockStateId(new Vec3(pos.x, pos.y, pos.z), stateId);
+    });
 
-    emitter.on('timeOfDay', ({ tick }) => {
-      this.setTimeOfDay(tick)
-    })
+    emitter.on("timeOfDay", ({ tick }) => {
+      this.setTimeOfDay(tick);
+    });
 
-    this.domElement.addEventListener('pointerdown', (evt) => {
-      const raycaster = new THREE.Raycaster()
-      const mouse = new THREE.Vector2()
-      mouse.x = (evt.clientX / this.domElement.clientWidth) * 2 - 1
-      mouse.y = -(evt.clientY / this.domElement.clientHeight) * 2 + 1
-      raycaster.setFromCamera(mouse, this.camera)
-      const ray = raycaster.ray
-      emitter.emit('mouseClick', { origin: ray.origin, direction: ray.direction, button: evt.button })
-    })
+    this.domElement.addEventListener("pointerdown", (evt) => {
+      const raycaster = new THREE.Raycaster();
+      const mouse = new THREE.Vector2();
+      mouse.x = (evt.clientX / this.domElement.clientWidth) * 2 - 1;
+      mouse.y = -(evt.clientY / this.domElement.clientHeight) * 2 + 1;
+      raycaster.setFromCamera(mouse, this.camera);
+      const ray = raycaster.ray;
+      emitter.emit("mouseClick", {
+        origin: ray.origin,
+        direction: ray.direction,
+        button: evt.button,
+      });
+    });
   }
 
-  update () {
-    TWEEN.update()
+  update() {
+    TWEEN.update();
   }
 
-  async waitForChunksToRender () {
-    await this.world.waitForChunksToRender()
+  async waitForChunksToRender() {
+    await this.world.waitForChunksToRender();
   }
 }
 
-module.exports = { Viewer }
+module.exports = { Viewer };
